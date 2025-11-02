b131c17b1d95d4cc21dd7ea00d2af5717a6b4bbb
Alexander Huaung <alex@avmlo.com>
2025-11-02 08:09:00 -0500

    fix: restore full iOS app functionality

--- ios-app/UrbanManual/App/UrbanManualApp.swift
+++ ios-app/UrbanManual/App/UrbanManualApp.swift
@@ -9,11 +9,17 @@ import SwiftUI
 
 @main
 struct UrbanManualApp: App {
+    @StateObject private var authViewModel = AuthViewModel()
+    
     var body: some Scene {
         WindowGroup {
-            Text("Hello World")
-                .font(.largeTitle)
-                .foregroundColor(.blue)
+            if authViewModel.isAuthenticated {
+                MainTabView()
+                    .environmentObject(authViewModel)
+            } else {
+                LoginView()
+                    .environmentObject(authViewModel)
+            }
         }
     }
 }
