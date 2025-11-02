fd0872a70ed20d51a2e00b91d854bb4fbf1760bf
Alexander Huaung <alex@avmlo.com>
2025-11-02 07:58:23 -0500

    chore: simplify iOS app to Hello World

--- ios-app/UrbanManual/App/UrbanManualApp.swift
+++ ios-app/UrbanManual/App/UrbanManualApp.swift
@@ -9,17 +9,11 @@ import SwiftUI
 
 @main
 struct UrbanManualApp: App {
-    @StateObject private var authViewModel = AuthViewModel()
-    
     var body: some Scene {
         WindowGroup {
-            if authViewModel.isAuthenticated {
-                MainTabView()
-                    .environmentObject(authViewModel)
-            } else {
-                LoginView()
-                    .environmentObject(authViewModel)
-            }
+            Text("Hello World")
+                .font(.largeTitle)
+                .foregroundColor(.blue)
         }
     }
 }
