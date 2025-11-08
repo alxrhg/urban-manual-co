d1033ff70777a270c41f2a8d456ecffce483f31a
avmlo <alex@avmlo.com>
2025-11-08 11:38:49 -0500

    Fix dependency array after removing recently viewed

--- app/destination/[slug]/page-client.tsx
+++ app/destination/[slug]/page-client.tsx
@@ -125,7 +125,7 @@ export default function DestinationPageClient({ initialDestination, parentDestin
       });
 
     }
-  }, [destination, addToRecentlyViewed]);
+  }, [destination]);
 
   useEffect(() => {
     if (destination) {
