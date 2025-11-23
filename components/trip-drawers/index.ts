103e9a0b3dbbb9545c67ce60343357b54b1e5916
alxrhg <62035034+alxrhg@users.noreply.github.com>
2025-11-23 09:46:04 -0500

    chore: refresh drawer scaffolding

---
+++ components/trip-drawers/index.ts
@@ -0,0 +1,7 @@
+export { default as TripOverviewDrawer } from "./TripOverviewDrawer";
+export { default as TripDayDrawer } from "./TripDayDrawer";
+export { default as TripAddMealDrawer } from "./TripAddMealDrawer";
+export { default as TripAddPlaceDrawer } from "./TripAddPlaceDrawer";
+export { default as TripAddHotelDrawer } from "./TripAddHotelDrawer";
+export { default as TripAISuggestionsDrawer } from "./TripAISuggestionsDrawer";
+export { default as TripReorderDrawer } from "./TripReorderDrawer";
