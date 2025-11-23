f2512f530d8b1d22d5ad82f19a2b4d4bd3168f14
alxrhg <62035034+alxrhg@users.noreply.github.com>
2025-11-23 10:01:24 -0500

    fix: enforce drawer type when opening

---
+++ components/trip-drawers/TripReorderDrawer.tsx
@@ -0,0 +1,3 @@
+export default function TripReorderDrawer() {
+  return null;
+}
---
+++ lib/stores/drawer-store.ts
@@ -0,0 +1,22 @@
+"use client";
+
+import { create } from "zustand";
+
+export type DrawerType = string | null;
+export type DrawerProps = Record<string, unknown>;
+
+interface DrawerStore {
+  open: boolean;
+  type: DrawerType;
+  props: DrawerProps;
+  openDrawer: (type: string, props?: DrawerProps) => void;
+  closeDrawer: () => void;
+}
+
+export const useDrawerStore = create<DrawerStore>((set) => ({
+  open: false,
+  type: null,
+  props: {},
+  openDrawer: (type, props = {}) => set({ open: true, type, props }),
+  closeDrawer: () => set({ open: false, type: null, props: {} }),
+}));
